670a6a3f9e02778e2359dc7e83b0056fa48ab088
정재훈 <70499531+pleasemrlostman@users.noreply.github.com>
2024-06-18 21:21:08 +0900

    feat: swagger 추가

--- src/users/schema/users.schema.ts
+++ src/users/schema/users.schema.ts
@@ -1,21 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Schema({ collection: 'users' })
 export class Users extends Document {
   @Prop({ required: true, unique: true })
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'user@example.com',
+  })
   email: string;
 
   @Prop({ required: true })
+  @ApiProperty({ description: 'The password of the user' })
   password: string;
 
   @Prop({ required: true })
+  @ApiProperty({ description: 'The name of the user' })
   name: string;
 
   @Prop({ unique: true })
+  @ApiProperty({ description: 'The nickname of the user', example: 'john_doe' })
   nickname: string;
 
   @Prop()
+  @ApiProperty({
+    description: "The URL of the user's profile image",
+    example: 'https://example.com/profile.jpg',
+  })
   profile_image: string;
 }
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('유저: Users') // 태그 추가
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('list')
+  @ApiOperation({ summary: '유저 리스트 받아오기' }) // 작업(Operation) 요약 추가
+  @ApiResponse({
+    status: 200,
+    description: 'List of users fetched successfully',
+  }) // 응답(Response) 설명 추가
   async getUsersList() {
     return this.usersService.getUserList();
   }
